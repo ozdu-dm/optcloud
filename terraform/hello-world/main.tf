89c10bbaf7d4843b36fae3aeb4a5359a3704e7d0
Oscar Ducuara <oducuara12@gmail.com>
2025-10-06 17:49:13 +0200

    Terraform comentado

--- terraform/hello-world/main.tf
+++ terraform/hello-world/main.tf
@@ -3,11 +3,11 @@ provider "aws" {
   region = "us-east-1"
 }
 #crear instancia de EC2
-resource "aws_instance" "hello_world" { 
-  ami           = "ami-052064a798f08f0d3" #AMI de Amazon Linux 2 en us-east-1
-  instance_type = "t2.micro"
+# resource "aws_instance" "hello_world" { 
+#   ami           = "ami-052064a798f08f0d3" #AMI de Amazon Linux 2 en us-east-1
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "terraform-primer-vistazo"
-  }
-}
+#   tags = {
+#     Name = "terraform-primer-vistazo"
+#   }
+# }
